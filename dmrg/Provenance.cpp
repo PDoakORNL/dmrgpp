08d56a4d2dd5dce9f1fba8b77b851cdbe54860a8
Gonzalo Alvarez <gz1@ornl.gov>
2026-07-14 13:16:04 -0400

    restored Provenance.cpp

--- dmrg/Provenance.cpp
+++ dmrg/Provenance.cpp
@@ -3,5 +3,6 @@
 std::ostream& operator<<(std::ostream& os, const Provenance&)
 {
 	os << "DMRG++ version " << DMRGPP_VERSION << " " << DMRGPP_GIT_REV << "\n";
+	os << "PsimagLite version " << PSIMAGLITE_VERSION << " " << PSIMAGLITE_GIT_REV << "\n";
 	return os;
 }
